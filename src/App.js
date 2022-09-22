1fd4f3659f4177f697814eb50f2769a45799a92b
Nargiz Rafiyeva <nargiz.pr@code.local>
2022-09-22 16:23:56 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import { Table } from 'antd';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const columns = [
+  {
+    title: 'Id',
+    dataIndex: 'id',
+    key: 'id',
+  },
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    sorter: (a, b) => a.name.length - b.name.length,
+  },
+  {
+    title: 'User Name',
+    dataIndex: 'username',
+    key: 'username',
+    sorter: (a, b) => a.username.length - b.username.length,
+  },
+  {
+    title: 'Email',
+    dataIndex: 'email',
+    key: 'email',
+  },
+  {
+    title: 'Address',
+    dataIndex: ['address', 'street'],
+    key: ['address', 'street'],
+  },
+];
+
+
 
 function App() {
+
+  const[users, setUsers] = useState([]);
+
+  console.log(users);
+  useEffect(() => {
+    (async() => {
+      const result = await axios('https://jsonplaceholder.typicode.com/users');
+      const response = result.data;
+      setUsers(response);
+    })()
+  }, [])
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Table dataSource={users} columns={columns} />
   );
 }
 
